272eeea63519d282d01fb6852c2a21f5e8dd6628
yuanning <yn394503599@gmail.com>
2016-11-04 18:03:25 +0800

    commit

--- algorithm/src/BinaryDepth.java
+++ algorithm/src/BinaryDepth.java
@@ -3,10 +3,44 @@
  */
 public class BinaryDepth {
 
-    public int maxDepth(TreeNode treeNode) {
+    static int count = 1;
 
-        return 0;
+    //待修改
+    public static int maxDepth(TreeNode treeNode) {
+        if (null == treeNode)
+            return 0;
+
+        TreeNode left = treeNode.left;
+        TreeNode right = treeNode.right;
+
+        if (null!=left || null!=right)
+            count++;
+
+        maxDepth(left);
+        maxDepth(right);
+        return count;
     }
+
+    public static void main(String[] args) {
+        /**
+         * initialize data
+         */
+//        TreeNode a = new TreeNode(1);
+//        TreeNode b = new TreeNode(3);
+//        TreeNode c = new TreeNode(2, a, b);
+//        TreeNode d = new TreeNode(6);
+//        TreeNode e = new TreeNode(9);
+//        TreeNode f = new TreeNode(7, d, e);
+//        TreeNode g = new TreeNode(4, c, f);
+
+
+        TreeNode a = new TreeNode(2, new TreeNode(4));
+        TreeNode b = new TreeNode(3, new TreeNode(5));
+        TreeNode c = new TreeNode(1, a, b);
+
+        System.out.println(BinaryDepth.maxDepth(c));
+    }
+
 
 }
 
--- algorithm/src/BinaryTree.java
+++ algorithm/src/BinaryTree.java
@@ -116,6 +116,10 @@ class TreeNode {
         this.val = val;
     }
 
+    public TreeNode(int val, TreeNode child) {
+        this(val, child, null);
+    }
+
     public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
